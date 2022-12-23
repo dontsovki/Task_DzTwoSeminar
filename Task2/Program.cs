dfc5eec9a538a5f2632183320a396443df2ba923
Artem.don <dontsovki@gmail.com>
2022-12-23 18:09:11 +0300

    Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет..

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-Console.WriteLine("Hello, World!");
+Console.Write("введите любое число ");
+string str = Console.ReadLine();
+char[] a=str.ToCharArray();
+
+int len = a.Length;
+if (len != 3)
+    Console.Write($"вы ввели {str} это число не трёхзначное");
+else
+    Console.Write($"Третье число {a[1]} из числа {str}");
